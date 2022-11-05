51b1f06ed1a7743164201f05c1dc0a987a35fd01
hyneq <hynekv.svobodny@gmail.com>
2022-11-05 18:00:51 +0100

    Added logging

--- extract_birds_cam_trap.sh
+++ extract_birds_cam_trap.sh
@@ -3,4 +3,4 @@
 project_root="$(dirname $0)"
 images=$project_root/images
 
-$project_root/detection_extract.py $images/'cam_trap/*/*.JPG' 'bird' $images/'cam_trap_extracted/$i.jpg'
+$project_root/detection_extract.py $images/'cam_trap/*/*.JPG' 'bird' $images/'cam_trap_extracted/$i.jpg' | tee $project_root/extract_birds_cam_trap.log
