c2fd768b4d477d12b5b0160d9cf33242ff132289
hyneq <hynekv.svobodny@gmail.com>
2022-11-19 00:16:39 +0100

    Added batches to extract_birds_cam_trap.sh

--- extract_birds_cam_trap.sh
+++ extract_birds_cam_trap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+batch="$1"
+
 project_root="$(dirname $0)"
 images=$project_root/images
-log=$project_root/logs/extract_birds_cam_trap-$(date -Iminutes)
+log=$project_root/logs/extract_birds_cam_trap-$batch-$(date -Iminutes)
 
-python3 -u $project_root/detection_extract.py $images/'cam_trap/*/*.JPG' 'bird' $images/'cam_trap_extracted/$i.jpg' | tee $log
+python3 -u $project_root/detection_extract.py $images/'cam_trap/'$batch'/*/*.JPG' 'bird' $images/'cam_trap_extracted/$i.jpg' | tee $log
