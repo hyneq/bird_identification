0e53bb4cf1f14249d1753c3823cdffde757f8229
hyneq <hynekv.svobodny@gmail.com>
2022-11-19 21:59:00 +0100

    Updated train_jizbirds.sh

--- train_jizbirds.sh
+++ train_jizbirds.sh
@@ -2,8 +2,10 @@
 
 project_root="$(dirname $0)"
 model_name=jizbirds
+model_dir=$project_root/models/$model_name
+class_names_path=$model_dir/classes.csv
 dataset_path=$project_root/images/jizbirds
-save_path=$project_root/models/jizbirds.h5
+save_path=$model_dir/$model_name.h5
 log=$project_root/logs/train_jizbirds-$(date -Iminutes)
 
 python3 -u $project_root/train.py -vv -d -a -t $model_name $dataset_path $save_path | tee $log
