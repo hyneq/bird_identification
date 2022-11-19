26343b3790053742348dda482c1575f63d8199c0
hyneq <hynekv.svobodny@gmail.com>
2022-11-19 21:40:18 +0100

    Added train_jizbirds.py

---
+++ train_jizbirds.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+project_root="$(dirname $0)"
+model_name=jizbirds
+dataset_path=$project_root/images/jizbirds
+save_path=$project_root/models/jizbirds.h5
+log=$project_root/logs/extract_birds_cam_trap-$batch-$(date -Iminutes)
+
+python3 -u $project_root/train.py -vv -d -a -t $model_name $dataset_path $save_path> $project
