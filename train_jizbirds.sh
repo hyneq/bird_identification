db26bec60c7555e11f003a2f5ce1d5a51658104c
hyneq <hynekv.svobodny@gmail.com>
2022-11-19 22:00:49 +0100

    Updated train_jizbirds.py

--- train_jizbirds.sh
+++ train_jizbirds.sh
@@ -8,4 +8,4 @@ dataset_path=$project_root/images/jizbirds
 save_path=$model_dir/$model_name.h5
 log=$project_root/logs/train_jizbirds-$(date -Iminutes)
 
-python3 -u $project_root/train.py -vv -d -a -t $model_name $dataset_path $save_path | tee $log
+python3 -u $project_root/train.py -vv -d -a -t $model_name $class_names_path $dataset_path $save_path | tee $log
