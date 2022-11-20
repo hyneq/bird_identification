83bfbaa99425f49cd2ad5f707a4903a29177ec41
hyneq <hynekv.svobodny@gmail.com>
2022-11-20 19:29:36 +0100

    Updated train_jizbirds.sh

--- train_jizbirds.sh
+++ train_jizbirds.sh
@@ -9,6 +9,4 @@ dataset_path=$project_root/images/jizbirds
 save_path=$model_dir/$model_name.h5
 log=$project_root/logs/train_jizbirds-$(date -Iminutes).log
 
-export TF_CPP_MIN_LOG_LEVEL=3
-
-python -u $project_root/train.py -vv -d -a -t $model_name $class_names_path $dataset_path $save_path | grep -v '^WARNING:tensorflow:' | tee $log
+python -u $project_root/train.py -vv -d -a -t $model_name $class_names_path $dataset_path $save_path | tee $log
