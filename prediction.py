315cbb2d5fd7a9af9565bce777a0ecc81202abef
hyneq <hynekv.svobodny@gmail.com>
2023-01-07 13:11:18 +0100

    Updated prediction.py

--- prediction.py
+++ prediction.py
@@ -1,23 +1,45 @@
-from typing import Optional, Union
+from typing import Optional, Generic, TypeVar, get_type_hints
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+import csv
 import numpy as np
 
 from abstract_classification import AbstractClassificationProcessor as ACP, get_acp
 
 @dataclass()
-class PredictionModel:
-    pass
-
-@dataclass()
 class PredictionModelConfig:
-    pass
+    classes_path: str
+
+TPredictionModelConfig = TypeVar("TPredictionModelConfig", bound=PredictionModelConfig)
+TPredictionModelInput = TypeVar("TPredictionModelInput")
+TPredictionModelOutput = TypeVar("TPredictionModelOutput")
+
+class PredictionModel(ABC, Generic[TPredictionModelConfig, TPredictionModelInput, TPredictionModelOutput]):
+
+    class_names: list[str]
+
+    @abstractmethod
+    def __init__(self, cfg: TPredictionModelConfig):
+        self.class_names = self.load_classes(cfg.classes_path)
+
+    @abstractmethod
+    def predict(self, input: TPredictionModelInput) -> TPredictionModelOutput:
+        pass
+
+    @staticmethod
+    def load_classes(classes_path: str):
+        with open(classes_path, newline='') as f:
+            reader = csv.reader(f, delimiter=',')
+            return {int(row[0]): row[1] for row in reader}
+
+TPredictionModel = TypeVar("TPredictionModel", bound=PredictionModel)
+
 
 class PredictionProcessor(ABC):
     __slots__: tuple
 
-    output: np.array
+    output: np.ndarray
 
     def __init__(self, output):
         self.output = output
@@ -32,44 +54,61 @@ class PredictionProcessorWithACP(PredictionProcessor):
     acp: ACP
 
     @classmethod
-    def with_acp(cls, acp: ACP):
+    def with_acp(cls, acp_: ACP):
         class cls_copy(cls):
-            pass
-
-        cls_copy.__name__ = cls.__name__
-        cls_copy.acp = acp
+            __name__ = cls.__name__
+            acp = acp_
         
         return cls_copy
 
+TPredictionProcessor = TypeVar("TPredictionProcessor", bound=PredictionProcessor)
 
-class Predictor(ABC):
+class Predictor(Generic[TPredictionModel, TPredictionModelConfig, TPredictionProcessor, TPredictionModelInput]):
 
-    classification_processor: type[PredictionProcessor]
+    model_cls: type[TPredictionModel]
+    
+    model: TPredictionModel
+
+    predciction_processor: type[TPredictionProcessor]
 
     def __init__(self,
-            model: PredictionModel
+            model: Optional[TPredictionModel]=None,
+            model_cfg: Optional[TPredictionModelConfig]=None
         ):
-        
-        self.model = model
 
-    @abstractmethod
-    def predict(self):
-        pass
+        if not model:
+            self.model = self.load_model(model_cfg)
+        else:
+            self.model = model
 
-class PredictorWithACP(ABC):
+    @classmethod
+    def load_model(cls, cfg: TPredictionModelConfig) -> TPredictionModel:
+        return cls.model_cls(cfg)
+
+    def predict(self, input: TPredictionModelInput):
+        output = self.model.predict(input)
+
+        return self.prediction_processor(output).process()
+
+
+class PredictorWithACP(Predictor[TPredictionModel, TPredictionModelConfig, TPredictionProcessor, TPredictionModelInput]):
+
+    acp: ACP
+
     def __init__(self,
-            model: PredictionModel,
-            acp: ACP
+            acp: ACP,
+            *args,
+            **kwargs
         ):
 
-        super().__init__(model)
+        super().__init__(*args, **kwargs)
 
-        self.classification_processor = self.classification_processor.with_acp(acp)
+        self.prediction_processor = self.prediction_processor.with_acp(acp)
 
 
 def get_predictor_factory(
         name: str,
-        predictor: type[Predictor],
+        predictor: type[PredictorWithACP],
         model_cls: type[PredictionModel],
         model_cfg_cls: type[PredictionModelConfig],
         DEFAULT_MODEL_CONFIG: PredictionModelConfig,
@@ -88,7 +127,7 @@ def get_predictor_factory(
             model = predictor.load_model(model_config)
 
         if not acp:
-            acp = get_acp(class_names=model.class_names, **acp_kwargs)
+            acp: acp_cls = get_acp(class_names=model.class_names, **acp_kwargs)
         
         return predictor(model=model, acp=acp)
     
