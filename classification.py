15ff24ce4868e2b2d6483a67ca8675b7280fe98d
hyneq <hynekv.svobodny@gmail.com>
2023-01-07 13:11:24 +0100

    Updated classification.py

--- classification.py
+++ classification.py
@@ -7,7 +7,7 @@ import csv
 from typing import Optional, Union
 from dataclasses import dataclass
 from threading import Lock
-from abc import ABC, abstractmethod
+from abc import ABCMeta
 import argparse
 from enum_actions import enum_action
 
@@ -16,22 +16,36 @@ from tensorflow import keras
 import numpy as np
 from PIL.Image import Image, open as open_image
 
+import prediction
 from abstract_classification import AbstractClassificationProcessor as ACP, DEFAULT_ACP, ClassificationMode, get_acp
 
 @dataclass()
-class ClassificationModel:
-    model: any
-    class_names: list[str]
-    model_lock: Lock
-
-@dataclass()
-class ClassificationModelConfig:
+class ClassificationModelConfig(prediction.PredictionModelConfig):
     model_path: str
-    classes_path: str
 
     @classmethod
     def from_dir(cls, path: str):
         return cls(model_path=os.path.join(path, "model.h5"), classes_path=os.path.join(path, "classes.csv"))
+
+class ClassificationModel(prediction.PredictionModel[ClassificationModelConfig, Image, np.ndarray]):
+    mdoel: keras.Model
+    model_lock: Lock
+
+    def __init__(self, cfg: ClassificationModelConfig):
+        self.model = keras.models.load_model(cfg.model_path)
+        self.model_lock = Lock()
+        super().__init__(cfg)
+    
+    def predict(self, input: Image) -> np.ndarray:
+        image = input.resize((224,224))
+        blob = keras.utils.img_to_array(image)
+        blob = np.expand_dims(blob,0)
+
+        with self.model_lock:
+            predictions = self.model.predict(blob)
+        
+        return predictions[0]
+    
 
 @dataclass
 class Result:
@@ -42,16 +56,7 @@ DEFAULT_MODEL_PATH = os.path.join(os.path.dirname(__file__), "models", "czbirds"
 
 DEFAULT_MODEL_CONFIG = ClassificationModelConfig.from_dir(DEFAULT_MODEL_PATH)
 
-class ClassificationProcessor:
-    __slots__ = [
-        "output"
-    ]
-
-    acp: ACP
-
-    def __init__(self, output):
-        self.output = output
-
+class ClassificationProcessor(prediction.PredictionProcessorWithACP):
     def get_results(self, classes) -> list:
         return [Result(class_, self.scores[class_]) for class_ in classes]
 
@@ -61,90 +66,25 @@ class ClassificationProcessor:
         classes = self.acp.get_filtered_classes(self.scores)
         
         return self.get_results(classes)
-    
-    @classmethod
-    def _class_copy(cls):
-        class ClassificationProcessorWithACP(cls):
-            pass
-        
-        return ClassificationProcessorWithACP
-    
-    @classmethod
-    def with_acp(cls, acp: ACP):
-        cls = cls._class_copy()
-        cls.acp = acp
 
-        return cls
+class ImageClassifier(prediction.PredictorWithACP[ClassificationModel, ClassificationModelConfig, ClassificationProcessor, Image]):
+    __slots__: tuple
 
-class ImageClassifier:
-    __slots__ = [
-        "classification_processor",
-        "model"
-    ]
+    model_cls = ClassificationModel
 
-    classification_processor_cls: type[ClassificationProcessor] = ClassificationProcessor
-
-    classification_processor: ClassificationProcessor
-
-    model: ClassificationModel
-
-    def __init__(
-            self, model_config: ClassificationModelConfig=DEFAULT_MODEL_CONFIG,
-            model: Optional[ClassificationModel]=None,
-            acp: Optional[ACP]=None,
-        ):
-
-        if not model:
-            model = self.load_model(model_config)
-
-        self.model = model
-
-        if not acp:
-            acp = DEFAULT_ACP()
-
-        self.classification_processor = self.classification_processor_cls.with_acp(acp)
-
-    @classmethod
-    def load_model(cls, cfg: ClassificationModelConfig) -> ClassificationModel:
-        return ClassificationModel(keras.models.load_model(cfg.model_path), cls.load_classes(cfg.classes_path), Lock())
-    
-    @staticmethod
-    def load_classes(classes_path: str):
-        with open(classes_path, newline='') as f:
-            reader = csv.reader(f, delimiter=',')
-            return {int(row[0]): row[1] for row in reader}
-    
-    def classify(self, image: Image) -> list:
-        image = image.resize((224,224))
-        blob = keras.utils.img_to_array(image)
-        blob = np.expand_dims(blob,0)
-
-        with self.model.model_lock:
-            predictions = self.model.model.predict(blob)
-
-        scores = predictions[0]
-
-        return self.classification_processor(scores).process()
+    prediction_processor = ClassificationProcessor
 
 class FileImageClassifier(ImageClassifier):
-    def classify(self, im_path: str):
-        return super().classify(open_image(im_path))
+    def predict(self, im_path: str):
+        return super().predict(open_image(im_path))
 
-def get_image_classifier(
-        model_config: ClassificationModelConfig=DEFAULT_MODEL_CONFIG,
-        model: Optional[ClassificationModel]=None,
-        classifier: type[ImageClassifier]=ImageClassifier,
-        acp: Optional[ACP]= None,
-        **acp_kwargs,
-    ) -> ImageClassifier:
-    
-    if not model:
-        model = classifier.load_model(model_config)
-
-    if not acp:
-        acp = get_acp(class_names=model.class_names, **acp_kwargs)
-    
-    return classifier(model=model, acp=acp)
+get_image_classifier = prediction.get_predictor_factory(
+    "get_image_classifier",
+    ImageClassifier,
+    ClassificationModel,
+    ClassificationModelConfig,
+    DEFAULT_MODEL_CONFIG
+)
 
 def classify_images(
         images: Union[list[str], list[Image], str, Image],
@@ -162,10 +102,11 @@ def classify_images(
         else:
             classifier = ImageClassifier
     
-    if type(classifier) is type:
-        classifier = get_image_classifier(*args, classifier=classifier, **kwargs)
+    classifier_type = type(classifier)
+    if classifier_type is type or classifier_type is ABCMeta:
+        classifier = get_image_classifier(*args, predictor=classifier, **kwargs)
     
-    return [classifier.classify(image) for image in images]
+    return [classifier.predict(image) for image in images]
 
 def main():
     parser = argparse.ArgumentParser()
