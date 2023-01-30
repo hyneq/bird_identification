c1951a83a49ed45b5257baaa20b14a102f1a03c9
hyneq <hynekv.svobodny@gmail.com>
2023-01-30 09:08:55 +0100

    Updated extract_birds_cam_trap_classify.sh

--- extract_birds_cam_trap_classify.sh
+++ extract_birds_cam_trap_classify.sh
@@ -13,6 +13,6 @@ target_path=$target_path_base'/$c/$i.jpg'
 
 mkdir -p $target_path
 $project_root/create_class_dirs.sh $classes_path $target_path_base
-mkdir $target_path/__not_recognized__
+mkdir $target_path_base/__not_recognized__
 
 python3 -u $project_root/detection_extract.py $images/'cam_trap/'$batch'/*/*.JPG' 'bird' $target_path | tee $log
