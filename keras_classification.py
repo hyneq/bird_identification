391358cc172664f71c9239058dc0c2a595c74b3f
hyneq <hynekv.svobodny@gmail.com>
2023-02-04 13:03:19 +0100

    Added keras_classification.py

---
+++ keras_classification.py
@@ -0,0 +1,8 @@
+import numpy as np
+from keras_classification import KerasPredictionModel
+
+class KerasClassificationModel(KerasPredictionModel[np.ndarray]):
+    __slots__: tuple
+
+    def get_output(self, predictions: np.ndarray) -> np.ndarray:
+        return predictions[0]
