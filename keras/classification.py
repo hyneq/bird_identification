3833c634db6edeca642d4fb99cc71bdd83661068
hyneq <hynekv.svobodny@gmail.com>
2023-02-06 17:10:09 +0100

    Added KerasClassificationModelConfig

--- keras/classification.py
+++ keras/classification.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-from .prediction import KerasPredictionModel
-from classification.models import ClassificationModel
+from prediction.models import PredictionModelConfigWithCls
+from .prediction import KerasPredictionModel, KerasModelConfig
+from classification.models import ClassificationModel, ClassificationModelConfig
 
 class KerasClassificationModel(KerasPredictionModel[np.ndarray], ClassificationModel):
     __slots__: tuple
 
     def get_output(self, predictions: np.ndarray) -> np.ndarray:
         return predictions[0]
+
+class KerasClassificationModelConfig(KerasModelConfig, ClassificationModelConfig, PredictionModelConfigWithCls[KerasClassificationModel]):
+    pass
