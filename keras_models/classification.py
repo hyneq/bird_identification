669423f7484bfb4e768c3fa13ccce46efccc0c4c
hyneq <hynekv.svobodny@gmail.com>
2023-02-11 15:35:26 +0100

    KerasClassificationModel now uses type alias for model output

--- keras_models/classification.py
+++ keras_models/classification.py
@@ -2,12 +2,12 @@ import numpy as np
 
 from prediction.models import PredictionModelConfigWithCls
 from .prediction import KerasPredictionModel, KerasModelConfig
-from classification.models import ClassificationModel, ClassificationModelConfig
+from classification.models import ClassificationModel, ClassificationModelConfig, ClassificationModelOutput
 
-class KerasClassificationModel(KerasPredictionModel[np.ndarray], ClassificationModel):
+class KerasClassificationModel(KerasPredictionModel[ClassificationModelOutput], ClassificationModel):
     __slots__: tuple
 
-    def get_output(self, predictions: np.ndarray) -> np.ndarray:
+    def get_output(self, predictions: np.ndarray) -> ClassificationModelOutput:
         return predictions[0]
 
 class KerasClassificationModelConfig(KerasModelConfig, ClassificationModelConfig, PredictionModelConfigWithCls[KerasClassificationModel]):
