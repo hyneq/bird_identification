c9abd483bc680e7da4f3206839d74ccf7d0387fd
hyneq <hynekv.svobodny@gmail.com>
2023-02-11 16:34:13 +0100

    Updated classification defaults file structure

--- defaults/classification.py
+++ defaults/classification.py
@@ -2,16 +2,15 @@ import os
 
 from classification.models import ClassificationModelConfig, ClassificationModel
 
-from keras_models.prediction import KerasModelConfig
 from keras_models.classification import KerasClassificationModel, KerasClassificationModelConfig
 
 DEFAULT_MODEL_CLS: type[ClassificationModel]
-
-DEFAULT_MODEL_PATH: str = os.path.join(os.path.dirname(__file__), os.path.pardir, "models", "czbirds")
 
 DEFAULT_MODEL_CONFIG: ClassificationModelConfig
 
 
 DEFAULT_MODEL_CLS = KerasClassificationModel
+
+DEFAULT_MODEL_PATH = os.path.join(os.path.dirname(__file__), os.path.pardir, "models", "czbirds")
 
 DEFAULT_MODEL_CONFIG = KerasClassificationModelConfig.from_dir(DEFAULT_MODEL_PATH)
