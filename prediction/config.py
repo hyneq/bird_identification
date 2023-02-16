4118f88121d22a949d11f062ffb169dc2efcc849
hyneq <hynekv.svobodny@gmail.com>
2023-02-16 20:47:31 +0100

    Updated PredictorConfig

--- prediction/config.py
+++ prediction/config.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, Generic
+from typing import Union, Generic
 
 from .models import TPredictionModelConfig
 from . import classes
@@ -7,5 +7,6 @@ from . import classes
 @dataclass
 class PredictorConfig(Generic[TPredictionModelConfig]):
     model_config: TPredictionModelConfig
-    min_confidence: float = classes.DEFAULT_MIN_CONFIDENCE
-    classification_mode: classes.ClassificationMode
+    min_confidence: float = None
+    classification_mode: classes.ClassificationMode = None
+    classes: Union[list[int],list[str]] = None
