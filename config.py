03f6f73726284f48a68897e88690814e16e031c9
hyneq <hynekv.svobodny@gmail.com>
2023-02-19 09:52:08 +0100

    merge_conf takes cfg_cls as an argument

--- config.py
+++ config.py
@@ -1,15 +1,26 @@
-from typing import Optional
-from dataclasses import is_dataclass
+from typing import Optional, Callable
+from dataclasses import fields, is_dataclass
 
-def merge_conf(func: function) -> function:
+def merge_conf(cfg_cls: type):
 
-    def inner(*args, cfg: Optional[type]=None, **kwargs):
-        if is_dataclass(cfg):
-            for attr, val in cfg.__dict__.items():
-                if kwargs.get(attr, None) is None:
-                    kwargs[attr] = val
-        
-        func(*args, **kwargs)
+    if not is_dataclass(cfg_cls):
+        raise TypeError("Config class must be a dataclass")
+    
+    cfg_fields = fields(cfg_cls)
 
+    def merge_conf_with_cls(func: Callable) -> Callable:
 
-    return inner
+        def inner(*args, cfg: Optional[cfg_cls]=None, **kwargs):
+            if cfg:
+                if not isinstance(cfg, cfg_cls):
+                    raise TypeError("Config object must be a {} instance".format(cfg_cls.__name__))
+
+                for field in cfg_fields:
+                    if kwargs.get(field.name, None) is None:
+                        kwargs[field.name] = getattr(cfg, field.name)
+            
+            return func(*args, **kwargs)
+
+        return inner
+    
+    return merge_conf_with_cls
