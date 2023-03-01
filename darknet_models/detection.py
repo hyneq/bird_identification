e7cf7634893735af54cb4ba6d51a1a1c8272ffe2
hyneq <hynekv.svobodny@gmail.com>
2023-03-01 20:46:34 +0100

    Added DARKNET_YOLOV3_DETECTION_MODEL_TYPE

--- darknet_models/detection.py
+++ darknet_models/detection.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from prediction.models import PredictionModelConfigWithCls
+from detection.models import DetectionModelType
 from .prediction import DarknetPredictionModel, DarknetPredictionModelConfig
 from YOLOv3_models.detection import YOLOv3DetectionModel, YOLOv3DetectionModelConfig, YOLOv3DetectionModelOutput, YOLOv3DetectionModelRawOutput
 
@@ -11,3 +12,9 @@ class DarknetYOLOv3DetectionModel(DarknetPredictionModel[YOLOv3DetectionModelOut
 
 class DarknetYOLOv3DetectionModelConfig(DarknetPredictionModelConfig, YOLOv3DetectionModelConfig, PredictionModelConfigWithCls[DarknetYOLOv3DetectionModel]):
     model_cls = DarknetYOLOv3DetectionModel
+
+DARKNET_YOLOV3_DETECTION_MODEL_TYPE = DetectionModelType(
+    name="darknet-YOLOv3",
+    model_cls=DarknetYOLOv3DetectionModel,
+    model_config_cls=DarknetPredictionModelConfig
+)
