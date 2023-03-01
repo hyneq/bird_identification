84f0da71f9db9df55efea63852801f2237784b44
hyneq <hynekv.svobodny@gmail.com>
2023-03-01 20:13:16 +0100

    Added ClasificationModelType

--- classification/models.py
+++ classification/models.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from prediction.models import PredictionModelConfig, AImagePredictionModel, get_prediction_model_factory
+from prediction.models import PredictionModelConfig, AImagePredictionModel, PredictionModelType, get_prediction_model_factory
 
 class ClassificationModelConfig(PredictionModelConfig):
     pass
@@ -10,12 +10,16 @@ ClassificationModelOutput = np.ndarray
 class ClassificationModel(AImagePredictionModel[ClassificationModelConfig, ClassificationModelOutput]):
     __slots__: tuple
 
+class ClassificationModelType(PredictionModelType[ClassificationModel, ClassificationModelConfig]):
+    pass
+
 from defaults.classification import DEFAULT_MODEL_CLS, DEFAULT_MODEL_CONFIG
 
 get_classification_model = get_prediction_model_factory(
     name="get_classification_model",
     model_cls=ClassificationModel,
     model_config_cls=ClassificationModelConfig,
+    model_type_cls=ClassificationModelType,
     DEFAULT_MODEL_CLS=DEFAULT_MODEL_CLS,
     DEFAULT_MODEL_CONFIG=DEFAULT_MODEL_CONFIG
 )
