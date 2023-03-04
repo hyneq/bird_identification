9358a54199670fb75c9e621c8e5ab34eb88bba56
hyneq <hynekv.svobodny@gmail.com>
2023-03-04 11:53:53 +0100

    Added model types to defaults.classification

--- defaults/classification.py
+++ defaults/classification.py
@@ -2,7 +2,7 @@ import os
 
 from classification.models import ClassificationModelConfig, ClassificationModel
 
-from keras_models.classification import KerasClassificationModel, KerasClassificationModelConfig
+from keras_models.classification import KerasClassificationModel, KerasClassificationModelConfig, KERAS_CLASSIFICATION_MODEL_TYPE
 
 DEFAULT_MODEL_CLS: type[ClassificationModel]
 
@@ -14,3 +14,9 @@ DEFAULT_MODEL_CLS = KerasClassificationModel
 DEFAULT_MODEL_PATH = os.path.join(os.path.dirname(__file__), os.path.pardir, "models", "czbirds")
 
 DEFAULT_MODEL_CONFIG = KerasClassificationModelConfig.from_dir(DEFAULT_MODEL_PATH)
+
+DEFAULT_MODEL_TYPE = KERAS_CLASSIFICATION_MODEL_TYPE
+
+CLASSIFICATION_MODEL_TYPES = [
+    KERAS_CLASSIFICATION_MODEL_TYPE
+]
