0343c22b23bf5703c3c81215b47911b9ce3d5160
hyneq <hynekv.svobodny@gmail.com>
2023-03-26 14:43:50 +0200

    Detection CLI uses prediction.PredictionCLI

--- cli/detection.py
+++ cli/detection.py
@@ -4,13 +4,20 @@ sys.path.remove(os.path.dirname(__file__))
 
 import numpy as np
 
-from detection.detector import detect_objects
+from detection.detector import file_object_detector_factory
 
+from cli.prediction import PredictionCLI
+
+def cli_main():
+    PredictionCLI(file_object_detector_factory).run()
+
+"""
 def cli_main():
     results = detect_objects(sys.argv[1])
 
     for result in results[0]:
         print("{} at {} with {} % confidence".format(result.label, result.bounding_box, np.round(result.confidence*100,2)))
+"""
 
 if __name__ == "__main__":
     cli_main()
