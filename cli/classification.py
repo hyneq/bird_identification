85fbbfb12fa6222c9ec9668a7cb20a23705b6468
hyneq <hynekv.svobodny@gmail.com>
2023-03-26 14:34:04 +0200

    Classification CLI uses prediction.PredictionCLI

--- cli/classification.py
+++ cli/classification.py
@@ -2,13 +2,14 @@ import os,sys
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 sys.path.remove(os.path.dirname(__file__))
 
-import argparse
+from classification.classifier import file_image_classifier_factory
 
-from enum_actions import enum_action
+from cli.prediction import PredictionCLI
 
-from classification.classifier import classify_images
-from prediction.classes import ClassificationMode
+def cli_main():
+    PredictionCLI(file_image_classifier_factory).run()
 
+"""
 def cli_main():
     parser = argparse.ArgumentParser()
 
@@ -28,6 +29,7 @@ def cli_main():
 
     for result in results:
         print(result)
+"""
 
 if __name__ == "__main__":
     cli_main()
