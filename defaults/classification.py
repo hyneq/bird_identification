f6955b1939a708e3ed98cd52a1b668d422a89af5
hyneq <hynekv.svobodny@gmail.com>
2023-04-08 22:29:27 +0200

    Removed unused constants from defaults

--- defaults/classification.py
+++ defaults/classification.py
@@ -1,17 +1,7 @@
-import os
-
-from classification.models import ClassificationModelConfig, ClassificationModel
-
 from keras_models.classification import KERAS_CLASSIFICATION_MODEL_FACTORY
-
-DEFAULT_MODEL_CLS: type[ClassificationModel]
-
-DEFAULT_MODEL_CONFIG: ClassificationModelConfig
 
 MODEL_FACTORIES = [
     KERAS_CLASSIFICATION_MODEL_FACTORY
 ]
 
 DEFAULT_MODEL_FACTORY = KERAS_CLASSIFICATION_MODEL_FACTORY.name
-
-DEFAULT_MODEL_PATH = os.path.join(os.path.dirname(__file__), os.path.pardir, "models", "czbirds")
--- defaults/detection.py
+++ defaults/detection.py
@@ -1,12 +1,4 @@
-import os
-
-from detection.models import DetectionModelConfig, DetectionModel
-
 from darknet_models.detection import DARKNET_YOLOV3_DETECTION_MODEL_FACTORY
-
-DEFAULT_MODEL_CLS: type[DetectionModel]
-
-DEFAULT_MODEL_CONFIG: DetectionModelConfig
 
 MODEL_FACTORIES = [
     DARKNET_YOLOV3_DETECTION_MODEL_FACTORY
@@ -14,7 +6,4 @@ MODEL_FACTORIES = [
 
 DEFAULT_MODEL_FACTORY = DARKNET_YOLOV3_DETECTION_MODEL_FACTORY.name
 
-DEFAULT_MODEL_PATH = os.path.join(os.path.dirname(__file__),os.path.pardir,"models","YOLOv3-COCO")
-
-DEFAULT_PROBABILITY_MINIMUM = 0.5
 DEFAULT_NMS_THRESHOLD = 0.5
