71ac90cae46c9fffb167e7e84db1056fc3c218a9
hyneq <hynekv.svobodny@gmail.com>
2023-04-08 22:21:42 +0200

    Removed deprecate cli functions

--- cli/classification.py
+++ cli/classification.py
@@ -9,27 +9,5 @@ from cli.prediction import PredictionCLI
 def cli_main():
     PredictionCLI(image_classifier_factory).run()
 
-"""
-def cli_main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-m", "--mode", action=enum_action(ClassificationMode), help="The classification mode to use")
-    parser.add_argument("--min-confidence", type=int, choices=range(0,100), help="Minimum confidence to find")
-    parser.add_argument("-c", "--class", type=str, nargs='*', help="class(es) to find", dest="classes")
-    parser.add_argument("images", type=str, nargs='+', help="The image file(s) to process")
-
-    args = parser.parse_args()
-
-    results = classify_images(
-        images=args.images,
-        mode=args.mode,
-        min_confidence=args.min_confidence,
-        classes=args.classes,
-    )
-
-    for result in results:
-        print(result)
-"""
-
 if __name__ == "__main__":
     cli_main()
--- cli/detection.py
+++ cli/detection.py
@@ -9,13 +9,5 @@ from cli.prediction import PredictionCLI
 def cli_main():
     PredictionCLI(object_detector_factory).run()
 
-"""
-def cli_main():
-    results = detect_objects(sys.argv[1])
-
-    for result in results[0]:
-        print("{} at {} with {} % confidence".format(result.label, result.bounding_box, np.round(result.confidence*100,2)))
-"""
-
 if __name__ == "__main__":
     cli_main()
