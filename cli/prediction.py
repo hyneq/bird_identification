ac94dadadce8a71b270bf3eab91c7a9025d3488c
hyneq <hynekv.svobodny@gmail.com>
2023-04-15 19:38:07 +0200

    Removed generics from prediction CLI

--- cli/prediction.py
+++ cli/prediction.py
@@ -73,7 +73,7 @@ class PredictionCLI(CLIWithParts):
 
     prediction_cli_part: PredictionCLIPart
 
-    def __init__(self, predictor_factory: predictor.IPredictorFactory[predictor.Predictor, predictor.PredictionModelT, predictor.PredictionModelInputT, predictor.PredictorConfigT, predictor.PredictionModelConfigT]):
+    def __init__(self, predictor_factory: predictor.IPredictorFactory):
         self.prediction_cli_part = prediction_cli_part = PredictionCLIPart(predictor_factory)
         super().__init__(parts=[prediction_cli_part])
     
