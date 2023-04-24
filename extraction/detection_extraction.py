9af5c7bfa21938342067c898f51d98b6d42348e3
hyneq <hynekv.svobodny@gmail.com>
2023-04-24 16:57:56 +0200

    Reduced and renamed to detection_extraction

---
+++ extraction/detection_extraction.py
@@ -0,0 +1,5 @@
+from prediction.image_utils import Image
+from detection.detector import DetectionResult
+
+def extract_detection(image: Image, result: DetectionResult) -> Image:
+    return image[result.bounding_box.range()]
