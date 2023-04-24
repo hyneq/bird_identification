563aa824b58ca68ba178921a8406420254e2df3a
hyneq <hynekv.svobodny@gmail.com>
2023-04-24 18:30:52 +0200

    Added detection_classification
    Class DetectionClassifier:
      - feeds an mage to ObjectDetector, which returns found objects
      - the extracted objects are fed to ImageClassifier for classification
      - a combined result is returned

---
+++ detection_classification/__init__.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+
+from prediction.classes import Scores
+from prediction.image_utils import Image
+from detection.detector import ObjectDetector, DetectionResult, DetectionResults, BoundingBox
+from classification.classifier import ImageClassifier, ClassificationResult
+from extraction.detection_extraction import extract_detection
+
+@dataclass
+class DetectionClassificationResult:
+    detection_result: DetectionResult
+    classification_result: ClassificationResult
+
+    @property
+    def bounding_box(self) -> BoundingBox:
+        return self.detection_result.bounding_box
+    
+    @property
+    def confidences(self) -> list[float]:
+        return self.classification_result.confidences
+    
+    @property
+    def class_names(self) -> list[str]:
+        return self.classification_result.class_names
+
+DetectionClassificationResults = list[DetectionClassificationResult]
+
+class DetectionClassifier:
+
+    detector: ObjectDetector
+    classifier: ImageClassifier
+
+    def __init__(self, detector: ObjectDetector, classifier: ImageClassifier):
+        self.detector = detector
+        self.classifier = classifier
+    
+    def predict(self, input: Image) -> list[DetectionClassificationResult]:
+        detection_results = self.detector.predict(input)
+
+        results: DetectionClassificationResults = []
+        for detection_result in detection_results:
+            extracted_image = extract_detection(input, detection_result)
+            classification_result = self.classifier.predict(extracted_image)
+
+            results.append(DetectionClassificationResult(
+                detection_result=detection_result,
+                classification_result=classification_result
+            ))
+        
+        return results
