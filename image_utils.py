8cc6db2252866a0d95f764cf557a60a220c33f74
hyneq <hynekv.svobodny@gmail.com>
2023-04-29 22:16:17 +0200

    Updated image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,8 +1,11 @@
+from typing_extensions import Self
 from dataclasses import dataclass
 import numpy as np
 import cv2
 
 Image = np.ndarray
+Point = tuple[int, int]
+Color = tuple[int, int, int]
 
 def load_img(img: str) -> Image:
     return cv2.imread(img)
@@ -18,4 +21,10 @@ class BoundingBox:
         return (
             np.arange(self.y, self.y + self.height),
             np.arange(self.x, self.x + self.width)
+        )
+    
+    def points(self) -> tuple[Point, Point]:
+        return (
+            (self.x, self.y),
+            (self.x+self.width, self.y+self.height)
         )
