cac94d34ced585db57956bbad5757edaa8092e29
hyneq <hynekv.svobodny@gmail.com>
2023-05-02 20:07:45 +0200

    Added file stream pair factories

--- streams/__init__.py
+++ streams/__init__.py
@@ -1,9 +1,9 @@
-from typing import Generic, TypeVar, Optional
+from typing import Generic, TypeVar, Optional, Callable
 from typing_extensions import Self 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
-from image_utils import Image
+from image_utils import Image, Size
 from . import IInStream, IOutStream
 
 StreamInputT = TypeVar("StreamInputT")
@@ -26,6 +26,13 @@ class IOutStream(Generic[StreamInputT], ABC):
     def write(self, frame: StreamInputT):
         pass
 
+class IFileStreamPairFactory(Generic[StreamInputT, StreamOutputT], ABC):
+
+    @abstractmethod
+    def __call__(self, in_path: str, out_path: str) -> tuple[IInStream[StreamInputT], IOutStream[StreamOutputT]]:
+        pass
+
+
 class IVideoInStream(IInStream[Image]):
 
     @property
@@ -39,3 +46,23 @@ class IVideoOutStream(IOutStream[Image]):
     @abstractmethod
     def size(self) -> tuple[int, int]:
         pass
+
+FileVideoInStreamFactory = Callable[[str], IVideoInStream]
+FileVideoOutStreamFactory = Callable[[str, Size], IVideoOutStream]
+
+from defaults.streams import DEFAULT_FILE_IN_STREAM_FACTORY, DEFAULT_FILE_OUT_STREAM_FACTORY, DEFAULT_FILE_IN_STREAM_PATH, DEFAULT_FILE_OUT_STREAM_PATH
+
+@dataclass
+class FileVideoStreamPairFactory(IFileStreamPairFactory[Image, Image]):
+    stream_in_factory: FileVideoInStreamFactory = DEFAULT_FILE_IN_STREAM_FACTORY
+    stream_out_factory: FileVideoOutStreamFactory = DEFAULT_FILE_OUT_STREAM_FACTORY
+    default_in_path: str = DEFAULT_FILE_IN_STREAM_PATH
+    default_out_path: str = DEFAULT_FILE_OUT_STREAM_PATH
+    
+    def __call__(self, in_path: str, out_path: str) -> tuple[IVideoInStream, IVideoOutStream]:
+        in_stream = self.stream_in_factory(in_path)
+        out_stream = self.stream_out_factory(out_path, in_stream.size)
+
+        return in_stream, out_stream
+
+get_file_video_stream_pair = FileVideoStreamPairFactory()
