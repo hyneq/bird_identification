60ea0addd38c2039ceceb6c2dac91f70284a43dc
hyneq <hynekv.svobodny@gmail.com>
2024-03-04 18:08:16 +0100

    Added fps, width and height arguments to streams.py
    These are used when the size of input stream can be defined (e.g. it is a camera)

--- bird_identification/cli/streams.py
+++ bird_identification/cli/streams.py
@@ -1,11 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Generic
+from typing import Generic, Optional
 
 from . import (
     ArgumentParser,
     ICLIPart,
     CLIPart,
-    Namespace,
     args_required,
 )
 
@@ -17,6 +16,8 @@ from ..streams import (
     MultiPathVideoStreamPairFactory,
     get_path_video_stream_pair,
 )
+
+from ..defaults.streams import DEFAULT_PATH_IN_STREAM_PATH, DEFAULT_PATH_OUT_STREAM_FACTORY
 
 
 class IStreamPairCLIPart(ICLIPart, ABC, Generic[InStreamT, OutStreamT]):
@@ -39,8 +40,8 @@ class MultiPathVideoStreamPairCLIPart(CLIPart, IStreamPairCLIPart):
         self.stream_pair_factory = stream_pair_factory
 
     def add_opts(self, parser: ArgumentParser):
-        parser.add_argument("-i", "--in", "--in-path", dest="in_path", required=True)
-        parser.add_argument("-o", "--out", "--out-path", dest="out_path", required=True)
+        parser.add_argument("-i", "--in", "--in-path", dest="in_path", default=DEFAULT_PATH_IN_STREAM_PATH)
+        parser.add_argument("-o", "--out", "--out-path", dest="out_path", default=DEFAULT_PATH_OUT_STREAM_FACTORY)
         parser.add_argument(
             "--in-type",
             dest="in_type",
@@ -51,10 +52,32 @@ class MultiPathVideoStreamPairCLIPart(CLIPart, IStreamPairCLIPart):
             dest="out_type",
             default=self.stream_pair_factory.stream_out_factory.default_factory,
         )
+        parser.add_argument(
+            "--fps", "--framerate",
+            dest="fps",
+            type=int,
+        )
+        parser.add_argument(
+            "--width", "--frame-width",
+            dest="width",
+        )
+        parser.add_argument(
+            "--height", "--frame-height",
+            dest="height",
+        )
 
     @args_required
     def get_stream_pair(self) -> tuple[IVideoInStream, IVideoOutStream]:
+        size = None
+        if self.args.width:
+            if not self.args.height:
+                raise ValueError("Width and height must both be specified or not.")
+            
+            size = (int(self.args.width), int(self.args.height))
+
         return self.stream_pair_factory(
+            fps=self.args.fps,
+            size=size,
             in_path=self.args.in_path,
             out_path=self.args.out_path,
             in_type=self.args.in_type,
