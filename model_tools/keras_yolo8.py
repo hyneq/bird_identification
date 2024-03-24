7497337d66bfa396261bb98af1b99aca5f10588c
hyneq <hynekv.svobodny@gmail.com>
2024-03-24 21:10:41 +0100

    Updated keras_yolo8.py

--- model_tools/keras_yolo8.py
+++ model_tools/keras_yolo8.py
@@ -8,13 +8,14 @@ import argparse
 import keras_cv
 
 DEFAULT_PRESET_NAME = "yolo_v8_m_pascalvoc"
+DEFAULT_NUM_CLASSES = 20
 
-def get_yolov8_model(preset_name=DEFAULT_PRESET_NAME) -> keras_cv.models.YOLOV8Detector:
+def get_yolov8_model(preset_name=DEFAULT_PRESET_NAME, num_classes=DEFAULT_NUM_CLASSES) -> keras_cv.models.YOLOV8Detector:
     """
     Creates a keras_cv.models.YOLOV8Detector from a preset
     """
 
-    return keras_cv.models.YOLOV8Detector.from_preset(preset_name)
+    return keras_cv.models.YOLOV8Detector.from_preset(preset_name, num_classes=num_classes, bounding_box_format="xywh")
 
 
 def main():
@@ -26,9 +27,15 @@ def main():
         description="Create a model file from a keras_cv.models.YOLOV8Detector preset"
     )
     parser.add_argument(
-        "--preset-name",
+        "--preset", "-p",
         default=DEFAULT_PRESET_NAME,
         help="The preset name to initialize the model with"
+    )
+    parser.add_argument(
+        "--num-classes", "-n",
+        default=DEFAULT_NUM_CLASSES,
+        help="The number of classes to initialize the model with",
+        type=int
     )
     parser.add_argument(
         "model_path",
@@ -37,7 +44,7 @@ def main():
 
     args = parser.parse_args()
 
-    model = get_yolov8_model(preset_name=args.preset_name)
+    model = get_yolov8_model(preset_name=args.preset, num_classes=args.num_classes)
     model.save(args.model_path)
 
 
