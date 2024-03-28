933e1481068f8ce2e51fac61df36a0d262383529
hyneq <hynekv.svobodny@gmail.com>
2024-03-28 23:45:39 +0100

    Log filename is now based on model_name

--- scripts/train_classification.sh
+++ scripts/train_classification.sh
@@ -8,6 +8,6 @@ model_dir="$project_root/models/$model_name"
 class_names_path="$model_dir/classes.txt"
 dataset_path="$project_root/images/$model_name"
 save_path="$model_dir/model.h5"
-log="$project_root/logs/train_jizbirds-$(date -Iminutes).log"
+log="$project_root/logs/train_$model_name-$(date -Iminutes).log"
 
 python3 -u "$project_root/training/train_keras_classification.py" -d -a -t "$model_name" "$class_names_path" "$dataset_path" "$save_path" | tee "$log"
