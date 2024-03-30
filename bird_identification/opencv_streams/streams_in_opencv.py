138c693f1b18354a0d8ede8dc561abe4997428bd
hyneq <hynekv.svobodny@gmail.com>
2024-03-30 11:31:59 +0100

    Fixed name factory not found

--- bird_identification/opencv_streams/streams_in_opencv.py
+++ bird_identification/opencv_streams/streams_in_opencv.py
@@ -4,3 +4,6 @@ from . import OpenCVVideoInStream
 
 def get_file_video_in_stream(path: str, *_, **__) -> OpenCVVideoInStream:
     return OpenCVVideoInStream(cv2.VideoCapture(path))
+
+
+factory = get_file_video_in_stream
