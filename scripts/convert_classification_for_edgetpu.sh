b95965b957b3af6ec27b2a61582573ab69f989d3
hyneq <hynekv.svobodny@gmail.com>
2024-04-01 01:42:19 +0200

    Renamed fixedbatchsize to fixedinputshape

--- scripts/convert_classification_for_edgetpu.sh
+++ scripts/convert_classification_for_edgetpu.sh
@@ -9,11 +9,11 @@ model_path="$project_root/models/$model_name"
 model_dataset="$project_root/images/$model_name"
 
 # Make batch size fixed
-"$project_root/training/keras_fix_input_shape.py" "$model_path/model.h5" "$model_path/model_fixedbatchsize.h5"
+"$project_root/training/keras_fix_input_shape.py" "$model_path/model.h5" "$model_path/model_fixedinputshape.h5"
 
 # Convert from Keras to quantized TF Lite model
 quantization="float_fallback_quant"
-"$project_root/training/keras_to_tflite.py" "$model_path/model_fixedbatchsize.h5" "$model_path/model.tflite" -o "$quantization" -d "$model_dataset"
+"$project_root/training/keras_to_tflite.py" "$model_path/model_fixedinputshape.h5" "$model_path/model.tflite" -o "$quantization" -d "$model_dataset"
 
 # Compile the TF Lite model for Edge TPU
 "$project_root/edgetpu_compiler/edgetpu_compiler" -o "$model_path" "$model_path/model.tflite"
