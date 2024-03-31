bfa5c889e230f1b47e495244e6d665c67f5faeb7
hyneq <hynekv.svobodny@gmail.com>
2024-03-31 13:42:33 +0200

    Added three optimization options to tflite conversion

--- training/keras_to_tflite.py
+++ training/keras_to_tflite.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+from typing import Optional
+
 import argparse
 
 import tensorflow as tf
 
-def convert(in_path: str, out_path: str, quantize: bool=False, ):
+def convert(in_path: str, out_path: str, *args, optimization: Optional[str]=None, **kwargs):
     """
     Converts a Keras model to a TF Lite model
     """
@@ -12,9 +14,10 @@ def convert(in_path: str, out_path: str, quantize: bool=False, ):
     keras_model = tf.keras.models.load_model(in_path)
 
     converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
+    converter.experimental_new_quantizer = True
 
-    if quantize:
-        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    if optimization:
+        OPTIMIZATIONS[optimization](converter, *args, **kwargs)
 
     tflite_model = converter.convert()
 
@@ -22,16 +25,53 @@ def convert(in_path: str, out_path: str, quantize: bool=False, ):
         f.write(tflite_model)
 
 
+def optimization_dynamic_range_quant(converter: tf.lite.TFLiteConverter, *_, **__):
+
+    # from https://www.tensorflow.org/lite/performance/post_training_quant
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+
+def optimization_float_fallback_quant(converter: tf.lite.TFLiteConverter, representative_data_path: str, *_, **__):
+
+    # from https://www.tensorflow.org/lite/performance/post_training_integer_quant
+
+    optimization_dynamic_range_quant(converter)
+
+    def representative_data_gen():
+        for input_value in (
+            tf.keras.preprocessing.image_dataset_from_directory(representative_data_path, labels=None, batch_size=1).take(100)
+        ):
+            yield [input_value[0]]
+
+    converter.representative_dataset = representative_data_gen
+
+
+def optimization_integer_only_quant(converter: tf.lite.TFLiteConverter, representative_data_path: str, *_, **__):
+
+    # from https://www.tensorflow.org/lite/performance/post_training_integer_quant
+
+    optimization_float_fallback_quant(converter, representative_data_path)
+
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    converter.inference_input_type = tf.uint8
+    converter.inference_output_type = tf.uint8
+
+
+OPTIMIZATIONS = {name.removeprefix("optimization_"): obj for name, obj in globals().items() if name.startswith("optimization_")}
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("in_path", help="path to the Keras model to convert")
     parser.add_argument("out_path", help="path where to save the converted TF Lite model")
-    parser.add_argument("-q", "--quantize", action="store_true",
-        help="quantize the converted model"
+    parser.add_argument("-o", "--optimization",
+        help="The optimization to use", choices=OPTIMIZATIONS.keys(), required=False
     )
+    parser.add_argument("-d", "--representative-data-path", help="The path to directory with representative data")
     args = parser.parse_args()
 
-    convert(args.in_path, args.out_path, quantize=args.quantize)
+    convert(args.in_path, args.out_path,
+        optimization=args.optimization, representative_data_path=args.representative_data_path
+    )
 
 
 if __name__ == "__main__":
