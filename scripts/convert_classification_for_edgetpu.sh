c1bc6927108c4c76e8c4bb784cdf167c7df9cf3c
hyneq <hynekv.svobodny@gmail.com>
2024-04-01 01:16:24 +0200

    Updated keras_fix_input_shape name

--- scripts/convert_classification_for_edgetpu.sh
+++ scripts/convert_classification_for_edgetpu.sh
@@ -9,7 +9,7 @@ model_path="$project_root/models/$model_name"
 model_dataset="$project_root/images/$model_name"
 
 # Make batch size fixed
-"$project_root/training/keras_fix_batch_size.py" "$model_path/model.h5" "$model_path/model_fixedbatchsize.h5"
+"$project_root/training/keras_fix_input_shape.py" "$model_path/model.h5" "$model_path/model_fixedbatchsize.h5"
 
 # Convert from Keras to quantized TF Lite model
 quantization="float_fallback_quant"
