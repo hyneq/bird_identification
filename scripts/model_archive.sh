4b01fc7ebcc2fb9429486853669c52549927b4df
hyneq <hynekv.svobodny@gmail.com>
2024-04-03 11:55:19 +0200

    Added model parent option to model_archive.sh

--- scripts/model_archive.sh
+++ scripts/model_archive.sh
@@ -7,7 +7,8 @@ source "$(dirname "$0")/../activate"
 model_name="$1"
 archive="archives/$model_name.zip"
 model_class_names="models/$model_name/classes.txt"
+model_parent="models/$model_name/parent.keras"
 model_dataset="images/$model_name"
 
 cd $project_root
-zip -r "$archive" "$model_class_names" "$model_dataset"
+zip -r "$archive" "$model_class_names" "$model_parent" "$model_dataset"
