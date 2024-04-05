11b0f09000d6754181e6733678d3a6c37ceb7d3b
hyneq <hynekv.svobodny@gmail.com>
2024-04-05 22:17:21 +0200

    Updated detection_models_tflite_YOLO

---
+++ bird_identification/tflite_models/detection_models_tflite_YOLO.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+from ..image_utils import Image
+
+from ..detection.models import DetectionModel, DetectionModelConfig, DetectionModelFactory
+
+from .prediction import TFLiteModelWithClassesConfig, TFLitePredictionModelWithClasses
+
+from .yolo import YOLODetectionModelOutput
+
+
+class TFLiteYOLODetectionModel(
+    TFLitePredictionModelWithClasses[Image, YOLODetectionModelOutput],
+    DetectionModel
+):
+
+    def set_input(self, input: Image):
+        input_shape = self.input_details[0]['shape'][1:3]
+        blob = cv2.dnn.blobFromImage(
+            input, 1 / 255.0, size=input_shape, swapRB=True, crop=False
+        )
+        blob = np.moveaxis(blob, 1, 3)
+        self.interpreter.set_tensor(self.input_details[0]['index'], blob)
+
+
+    def get_output(self, input: Image) -> YOLODetectionModelOutput:
+        raw_output = self.interpreter.get_tensor(self.output_details[0]["index"])[0].transpose()
+        height, width = input.shape[0:2]
+
+
+        return YOLODetectionModelOutput(raw_output, width, height)
+
+
+class TFLiteYOLODetectionModelConfig(
+    TFLiteModelWithClassesConfig, DetectionModelConfig
+):
+    pass
+
+
+factory = DetectionModelFactory[
+    str, TFLiteYOLODetectionModelConfig
+](
+    name="tflite-YOLO",
+    model_cls=TFLiteYOLODetectionModel,
+    model_config_cls=TFLiteYOLODetectionModelConfig,
+    model_config_loader=TFLiteYOLODetectionModelConfig.from_path,
+    default_model_config_input="models/YOLO-COCO",
+)
--- bird_identification/tflite_models/yolo.py
+++ bird_identification/tflite_models/yolo.py
@@ -1,51 +1,74 @@
-from typing import Iterator
-
 import numpy as np
 
+from ..prediction.classes import VectorScores
+
 from ..detection.models import (
-    DetectionModelOutput,
-    DetectionModelOutputIter
+    DetectionModelOutputIter,
+    DetectionModelOutput
 )
 
 
-DetectionObj = int
-DetectionRawOutput = tuple[np.ndarray, np.ndarray]
+YOLODetectionObj = np.ndarray
+YOLODetectionModelRawOutput = np.ndarray
 
 
-class YOLOv3DetectionModelOutputIter(
-    DetectionModelOutputIter[DetectionObj, DetectionRawOutput]
+class YOLODetectionModelOutputIter(
+    DetectionModelOutputIter[YOLODetectionObj, YOLODetectionModelRawOutput]
 ):
-    def __init__(self, raw_output: DetectionRawOutput):
-        self._range_iterator = iter(range(len(raw_output[0])))
-    
-    def __next__(self):
-        return next(self._range_iterator)
+    __slots__: tuple
+
+    _len: int
+    _index: int
+
+    def __init__(self, raw_output: YOLODetectionModelRawOutput):
+        super().__init__(raw_output)
+
+        self._len = raw_output.shape[0]
+        self._index = 0
+
+    def __next__(self) -> YOLODetectionObj:
+        if self._index >= self._len:
+            raise StopIteration
+
+        obj = self.raw_output[self._index]
+
+        self._index += 1
+
+        return obj
 
 
-class YOLOv3DetectionModelOutput(
+class YOLODetectionModelOutput(
     DetectionModelOutput[
-        DetectionObj,
-        DetectionRawOutput,
-        YOLOv3DetectionModelOutputIter
+        YOLODetectionObj,
+        YOLODetectionModelRawOutput,
+        YOLODetectionModelOutputIter,
     ]
 ):
-    iter_cls = YOLOv3DetectionModelOutputIter
+    iter_cls = YOLODetectionModelOutputIter
 
-    boxes: np.ndarray
-    scores: np.ndarray
+    width: int
+    height: int
 
-    def __init__(self, boxes: np.ndarray, scores: np.ndarray):
-        super().__init__((boxes, scores))
+    def __init__(
+        self, raw_output: YOLODetectionModelRawOutput, width: int, height: int
+    ):
+        super().__init__(raw_output)
 
-        self.boxes = boxes
-        self.scores = scores
+        self.width = width
+        self.height = height
 
+    def get_box(self, obj: YOLODetectionObj) -> tuple:
+        box = obj[0:4] * np.array([self.width, self.height, self.width, self.height])
 
-    def get_box(self, obj: DetectionObj) -> tuple:
-        x_min, y_min, x_max, y_max = self.boxes[obj]
+        x_center, y_center, box_width, box_height = box
+        x_min = max(int(x_center - (box_width / 2)), 0)
+        y_min = max(int(y_center - (box_height / 2)), 0)
+        if x_min + box_width >= self.width:
+            box_width = self.width - x_min
+        if y_min + box_height >= self.height:
+            box_height = self.width - y_min
 
-        return (round(x_min), round(y_min), round(x_max - x_min), round(y_max - y_min))
+        return (x_min, y_min, int(box_width), int(box_height))
 
-
-    def get_scores(self, obj: DetectionObj) -> np.ndarray:
-        return self.scores[obj]
+    def get_scores(self, obj: YOLODetectionObj) -> VectorScores:
+        return VectorScores(obj[4:])
