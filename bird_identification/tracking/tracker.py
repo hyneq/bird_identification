8ea81d576ebe566c3d81e79baee4ea21bcac5147
hyneq <hynekv.svobodny@gmail.com>
2024-04-13 18:00:47 +0200

    Updated tracking package

--- bird_identification/tracking/tracker.py
+++ bird_identification/tracking/tracker.py
@@ -1,4 +1,4 @@
-from typing import Generic, Optional
+from typing import Generic, TypeVar, Any, Protocol, Optional
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
@@ -18,62 +18,135 @@ class ITracker(ABC, Generic[PredictionResultT]):
 
 
 class ITrackerFactory(IFactory[ITracker[PredictionResultT]]):
-    pass
-
-
-class ALoggingTrackingLogic(ABC, Generic[PredictionResultT, LoggedObjectT]):
     __slots__: tuple
 
-    tracker: "LoggingTracker[PredictionResultT, LoggedObjectT]"
+
+TrackingLogicInputT = TypeVar("TrackingLogicInputT")
+
+class ITrackingLogic(ABC, Generic[TrackingLogicInputT]):
+    __slots__: tuple
+
+    tracker: "Tracker[Any, TrackingLogicInputT]"
 
     @abstractmethod
-    def update(self, results: PredictionResultT):
+    def update(self,
+        result: TrackingLogicInputT
+    ):
         pass
 
+
+class ITrackingLogicFactory(IFactory[ITrackingLogic[TrackingLogicInputT]]):
+    __slots__: tuple
+
+
+class IPredictionParser(Protocol, Generic[PredictionResultT, TrackingLogicInputT]):
+
+    @abstractmethod
+    def __call__(self, result: PredictionResultT) -> TrackingLogicInputT:
+        pass
+
+DEFAULT_PREDICTION_PARSER: IPredictionParser = lambda result: result
+
+
+class Tracker(ITracker[PredictionResultT], Generic[PredictionResultT, TrackingLogicInputT]):
+    __slots__: tuple
+
+    logic: ITrackingLogic[TrackingLogicInputT]
+    prediction_parser: IPredictionParser[PredictionResultT, TrackingLogicInputT]
+
+    def __init__(self,
+        logic: ITrackingLogic[TrackingLogicInputT], 
+        prediction_parser: IPredictionParser[PredictionResultT, TrackingLogicInputT] = DEFAULT_PREDICTION_PARSER
+    ):
+        self.logic = logic
+        self.prediction_parser = prediction_parser
+
+        logic.tracker = self
+
+
+    def _parse(self, result: PredictionResultT) -> TrackingLogicInputT:
+        return self.prediction_parser(result)
+
+
+    def update(self, 
+        result: PredictionResultT
+    ):
+        self.logic.update(self._parse(result))
+
+
+@dataclass(frozen=True)
+class TrackerFactory(IFactory[Tracker[PredictionResultT, TrackingLogicInputT]]):
+
+    logic_factory: ITrackingLogicFactory[TrackingLogicInputT]
+    default_prediction_parser: IPredictionParser[PredictionResultT, TrackingLogicInputT]
+
+    def __call__(self,
+        *args,
+        logic: Optional[ITrackingLogic[TrackingLogicInputT]] = None,
+        prediction_parser: Optional[IPredictionParser[PredictionResultT, TrackingLogicInputT]] = None,
+        **kwargs
+    ) -> Tracker[PredictionResultT, TrackingLogicInputT]:
+        if not logic:
+            logic = self.logic_factory(*args, **kwargs)
+        
+        if not prediction_parser:
+            prediction_parser = self.default_prediction_parser
+        
+        return Tracker(logic, prediction_parser)
+
+
+class ILoggingTrackingLogic(ITrackingLogic[TrackingLogicInputT], Generic[TrackingLogicInputT, LoggedObjectT]):
+    __slots__: tuple
+
+    tracker: "LoggingTracker[Any, TrackingLogicInputT, LoggedObjectT]"
 
     def log(self, objects: list[LoggedObjectT]):
         self.tracker.log(objects)
 
 
-class ALoggingTrackingLogicFactory(IFactory[ALoggingTrackingLogic[PredictionResultT, LoggedObjectT]]):
+class ILoggingTrackingLogicFactory(IFactory[ILoggingTrackingLogic[TrackingLogicInputT, LoggedObjectT]]):
     pass
 
 
-class LoggingTracker(ITracker[PredictionResultT], Generic[PredictionResultT, LoggedObjectT]):
+class LoggingTracker(Tracker[PredictionResultT, TrackingLogicInputT], Generic[PredictionResultT, TrackingLogicInputT, LoggedObjectT]):
     __slots__: tuple
 
-    logic: ALoggingTrackingLogic[PredictionResultT, LoggedObjectT]
+    logic: ILoggingTrackingLogic[TrackingLogicInputT, LoggedObjectT]
     logger: IObjectLogger[LoggedObjectT]
 
     def __init__(self,
-        logic: ALoggingTrackingLogic[PredictionResultT, LoggedObjectT],
-        logger: IObjectLogger[LoggedObjectT]
+        logic: ILoggingTrackingLogic[TrackingLogicInputT, LoggedObjectT],
+        logger: IObjectLogger[LoggedObjectT],
+        prediction_parser: IPredictionParser[PredictionResultT, TrackingLogicInputT] = DEFAULT_PREDICTION_PARSER,
     ):
-        self.logic = logic
+        super().__init__(logic, prediction_parser)
+
         self.logger = logger
-        
-        logic.tracker = self
-    
-
-    def update(self, result: PredictionResultT):
-        self.logic.update(result)
-
 
     def log(self, objects: list[LoggedObjectT]):
         self.logger.log(objects)
 
 
 @dataclass(frozen=True)
-class LoggingTrackerFactory(ITrackerFactory[PredictionResultT], Generic[PredictionResultT, LoggedObjectT]):
+class LoggingTrackerFactory(TrackerFactory[PredictionResultT, TrackingLogicInputT], Generic[PredictionResultT, TrackingLogicInputT, LoggedObjectT]):
 
-    logic_factory: ALoggingTrackingLogicFactory[PredictionResultT, LoggedObjectT]
+    logic_factory: ILoggingTrackingLogicFactory[TrackingLogicInputT, LoggedObjectT]
     logger_factory: IObjectLoggerFactory[LoggedObjectT]
 
     def __call__(self,
-        logic: Optional[ALoggingTrackingLogic[PredictionResultT, LoggedObjectT]] = None,
-        logger: Optional[IObjectLogger[LoggedObjectT]] = None
-    ):
-        logic = logic or self.logic_factory()
-        logger = logger or self.logger_factory()
+        *args,
+        logic: Optional[ILoggingTrackingLogic[TrackingLogicInputT, LoggedObjectT]] = None,
+        logger: Optional[IObjectLogger[LoggedObjectT]] = None,
+        prediction_parser: Optional[IPredictionParser[PredictionResultT, TrackingLogicInputT]] = None,
+        **kwargs
+    ) -> LoggingTracker[PredictionResultT, TrackingLogicInputT, LoggedObjectT]:
+        if not logic:
+            logic = self.logic_factory(*args, **kwargs)
+        
+        if not logger:
+            logger = self.logger_factory()
 
-        return LoggingTracker(logic, logger)
+        if not prediction_parser:
+            prediction_parser = self.default_prediction_parser
+        
+        return LoggingTracker(logic, logger, prediction_parser)
