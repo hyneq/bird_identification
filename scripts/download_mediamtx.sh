5e7b413274becd7bad3fbcedea193d140b21f839
hyneq <hynekv.svobodny@gmail.com>
2024-04-28 19:25:43 +0200

    Fixed wrong amr architecture name

--- scripts/download_mediamtx.sh
+++ scripts/download_mediamtx.sh
@@ -4,7 +4,7 @@ source "$(dirname "$0")/../vars"
 
 mediamtx_version="v1.8.0"
 mediamtx_system="$(uname -s | tr '[:upper:]' '[:lower:]')"
-mediamtx_arch="$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)"
+mediamtx_arch="$(uname -m | sed s/aarch64/arm64v8/ | sed s/x86_64/amd64/)"
 
 mediamtx_download_url="https://github.com/bluenviron/mediamtx/releases/download/$mediamtx_version/mediamtx_${mediamtx_version}_${mediamtx_system}_${mediamtx_arch}.tar.gz"
 
