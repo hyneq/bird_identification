912b27de87a9748823af17429b7b11d954372193
hyneq <hynekv.svobodny@gmail.com>
2024-05-03 01:15:09 +0200

    Added app.sh script and corresponding vars files

---
+++ scripts/app.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source "$(dirname "$0")/../activate"
+
+source "$project_root/config/vars"
+
+test -z "$rtsp_path" && rtsp_path="rtsp://localhost:$rtsp_port/prediction"
+
+
+python3 cli/app.py \
+    --width "$camera_width" --height "$camera_height" \
+    --out-type ffmpeg_rtsp -o rtsp://localhost:8554/prediction \
+    --logger-type sqlalchemy --log-path "$db_path" \
+    @"$app_args_file"
