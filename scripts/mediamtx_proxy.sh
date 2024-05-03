c6b77e8b2e1e585ba1f811be9bb18f80d8416aae
hyneq <hynekv.svobodny@gmail.com>
2024-05-03 09:54:21 +0200

    Removed mediamtx_proxy.sh as not needed in this repo

--- scripts/mediamtx_proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "$(dirname "$0")/../vars"
-
-export MTX_PATHS_PREDICTION_SOURCE="$REMOTE_STREAM_PATH"
-export MTX_PATHS_PREDICTION_SOURCEONDEMAND=yes
-
-exec $project_root/mediamtx
