4c66c897681105d3aff11bf434f47c0e13fb37a3
hyneq <hynekv.svobodny@gmail.com>
2024-05-05 04:55:20 +0200

    Added change directory to launch scripts

--- scripts/app.sh
+++ scripts/app.sh
@@ -6,6 +6,7 @@ source "$project_root/config/vars"
 
 test -z "$rtsp_path" && rtsp_path="rtsp://localhost:$rtsp_port/prediction"
 
+cd "$project_root"
 
 exec python3 cli/app.py \
     --width "$camera_width" --height "$camera_height" \
--- scripts/logged_objects_upload.sh
+++ scripts/logged_objects_upload.sh
@@ -4,6 +4,8 @@ source "$(dirname "$0")/../activate"
 
 source "$project_root/config/vars"
 
+cd "$project_root"
+
 exec python3 cli/logged_objects_upload.py \
     "$db_path" \
     "$remote_api_url"
