459f9df29d928327779e008fc55850e5b318cd83
hyneq <hynekv.svobodny@gmail.com>
2024-05-05 02:35:29 +0200

    Added logged_objects_upload.sh

---
+++ scripts/logged_objects_upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source "$(dirname "$0")/../activate"
+
+source "$project_root/config/vars"
+
+exec python3 cli/logged_objects_upload.py \
+    "$db_path" \
+    "$remote_api_url"
+    @"$remote_api_credentials"
